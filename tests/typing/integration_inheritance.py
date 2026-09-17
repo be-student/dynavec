65d410a44cfaebf5e567b614178d9750a7bee172
eunwoo song <presentsong@naver.com>
2026-09-17 21:15:52 +0900

    fix: preserve DSPy retrieval return contract

--- tests/typing/integration_inheritance.py
+++ tests/typing/integration_inheritance.py
@@ -1,5 +1,8 @@
+from typing import assert_type
+
 import dspy
 from dspy.predict.parameter import Parameter
+from dspy.primitives.prediction import Prediction
 from langchain_core.vectorstores import VectorStore
 from llama_index.core.vector_stores.types import BasePydanticVectorStore
 
@@ -21,6 +24,15 @@ def use_dspy(retriever: DynavecRM) -> dspy.Retrieve:
 
 def use_dspy_parameter(retriever: DynavecRM) -> Parameter:
     return retriever
+
+
+def check_dspy_return_contract(retriever: DynavecRM) -> None:
+    assert_type(retriever.forward("query"), list[Prediction])
+    base: dspy.Retrieve = retriever
+    assert_type(
+        base.forward("query"),
+        list[str] | Prediction | list[Prediction],
+    )
 
 
 def use_llamaindex(store: DynavecLlamaStore) -> BasePydanticVectorStore:
